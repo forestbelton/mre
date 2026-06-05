7bb0907864b4e4ec0ba8c534cdb3bb609727a89c
Forest Belton <65484+forestbelton@users.noreply.github.com>
2026-06-05 16:47:41 -0700

    Remove explicit SECTION directives for Kalum and Rafaga graphics

--- src/gfx/portrait/kalum.asm
+++ src/gfx/portrait/kalum.asm
@@ -10,28 +10,44 @@
 ; (loaded by Kalum_StartEncounter from $1d:$5800/$5840) are carved here from the
 ; PNG's colour table, so the portrait carries its real colour.
 
-SECTION "KalumPortraitTiles", ROMX[$4000], BANK[$1d]
+SECTION "Kalum graphics", ROMX
+
 KalumPortraitTiles:
 	INCBIN "assets/kalum/tiles.bin"    ; 384 tiles -> VRAM bank 1 $8000
 
-SECTION "KalumPortraitPaletteBg", ROMX[$5800], BANK[$1d]
 KalumPortraitPaletteBg:
 	INCBIN "assets/kalum/palette_bg.bin"   ; 6 BG palettes (RGB555 LE)
 
-SECTION "KalumPortraitPaletteObj", ROMX[$5840], BANK[$1d]
+DS $10, 0
+
 KalumPortraitPaletteObj:
 	INCBIN "assets/kalum/palette_obj.bin"  ; 6 OBJ palettes (RGB555 LE)
 
-SECTION "KalumPortraitMapDesc", ROMX[$5880], BANK[$1d]
+DS $10, 0
+
 KalumPortraitMapDesc:
 	db 11, 20                                    ; rows, cols
 	dw KalumPortraitAttrMap                       ; CGB attribute map pointer
 	dw KalumPortraitIndexMap                      ; tile index map pointer
 
-SECTION "KalumPortraitIndexMap", ROMX[$5886], BANK[$1d]
 KalumPortraitIndexMap:
 	INCBIN "assets/kalum/tilemap.bin"  ; 20x11 tile indices
 
-SECTION "KalumPortraitAttrMap", ROMX[$5962], BANK[$1d]
 KalumPortraitAttrMap:
 	INCBIN "assets/kalum/attrmap.bin"  ; 20x11 CGB BG attributes
+
+Data_1d_5a3e:
+	db $02, $03, $4a, $5a, $44, $5a, $ec, $f4, $fc, $ed, $f5, $fd, $0b, $0b, $0b, $0a
+	db $0a, $0a, $0c, $00, $00, $04, $08, $00, $08, $06, $08, $00, $10, $08, $08, $00
+	db $18, $0a, $08, $10, $00, $0c, $08, $10, $08, $0e, $08, $10, $10, $10, $08, $10
+	db $18, $12, $08, $20, $00, $14, $08, $20, $08, $16, $08, $20, $10, $18, $08, $20
+	db $18, $1a, $08, $02, $03, $8d, $5a, $87, $5a, $56, $5e, $60, $57, $5f, $61, $0b
+	db $0b, $0b, $0b, $0b, $0b, $0c, $00, $00, $04, $08, $00, $08, $06, $08, $00, $10
+	db $08, $08, $00, $18, $0a, $08, $10, $00, $0c, $08, $10, $08, $20, $08, $10, $10
+	db $22, $08, $10, $18, $24, $08, $20, $00, $14, $08, $20, $08, $16, $08, $20, $10
+	db $18, $08, $20, $18, $1a, $08, $02, $03, $d0, $5a, $ca, $5a, $62, $64, $66, $63
+	db $65, $67, $0b, $0b, $0b, $0b, $0b, $0b, $0c, $00, $00, $04, $08, $00, $08, $06
+	db $08, $00, $10, $08, $08, $00, $18, $0a, $08, $10, $00, $0c, $08, $10, $08, $26
+	db $08, $10, $10, $28, $08, $10, $18, $2a, $08, $20, $00, $14, $08, $20, $08, $16
+	db $08, $20, $10, $18, $08, $20, $18, $1a, $08, $02, $00, $00, $1c, $08, $00, $08
+	db $1e, $08, $02, $00, $00, $00, $0d, $00, $08, $02, $0d
--- src/gfx/portrait/rafaga.asm
+++ src/gfx/portrait/rafaga.asm
@@ -6,28 +6,40 @@
 ; palettes (loaded by Rafaga_StartEncounter from $1d:$7319/$7359) are carved here
 ; from the PNG's colour table.
 
-SECTION "RafagaPortraitTiles", ROMX[$5b19], BANK[$1d]
+SECTION "Rafaga graphics", ROMX
+
 RafagaPortraitTiles:
 	INCBIN "assets/rafaga/tiles.bin"
 
-SECTION "RafagaPortraitPaletteBg", ROMX[$7319], BANK[$1d]
 RafagaPortraitPaletteBg:
 	INCBIN "assets/rafaga/palette_bg.bin"   ; 6 BG palettes (RGB555 LE)
 
-SECTION "RafagaPortraitPaletteObj", ROMX[$7359], BANK[$1d]
+DS $10
+
 RafagaPortraitPaletteObj:
 	INCBIN "assets/rafaga/palette_obj.bin"  ; 6 OBJ palettes (RGB555 LE)
 
-SECTION "RafagaPortraitMapDesc", ROMX[$7399], BANK[$1d]
+DS $10
+
 RafagaPortraitMapDesc:
 	db 11, 20
 	dw RafagaPortraitAttrMap
 	dw RafagaPortraitIndexMap
 
-SECTION "RafagaPortraitIndexMap", ROMX[$739f], BANK[$1d]
 RafagaPortraitIndexMap:
 	INCBIN "assets/rafaga/tilemap.bin"
 
-SECTION "RafagaPortraitAttrMap", ROMX[$747b], BANK[$1d]
 RafagaPortraitAttrMap:
 	INCBIN "assets/rafaga/attrmap.bin"
+
+Data_1d_7557:
+	db $02, $02, $61, $75, $5d, $75, $ed, $f5, $ee, $f6, $0d, $0d, $0d, $0d, $0a, $00
+	db $00, $00, $08, $00, $08, $02, $08, $00, $10, $04, $08, $00, $18, $06, $08, $00
+	db $20, $08, $08, $10, $00, $0a, $08, $10, $08, $0c, $08, $10, $10, $0e, $08, $10
+	db $18, $10, $08, $10, $20, $12, $08, $02, $02, $98, $75, $94, $75, $56, $5e, $57
+	db $5f, $0d, $0d, $0d, $0d, $0a, $00, $00, $00, $08, $00, $08, $02, $08, $00, $10
+	db $26, $08, $00, $18, $28, $08, $00, $20, $08, $08, $10, $00, $0a, $08, $10, $08
+	db $0c, $08, $10, $10, $0e, $08, $10, $18, $10, $08, $10, $20, $12, $08, $06, $00
+	db $00, $14, $09, $00, $08, $16, $09, $00, $10, $18, $09, $10, $00, $1a, $09, $10
+	db $08, $1c, $09, $10, $10, $1e, $09, $02, $00, $00, $20, $09, $00, $08, $22, $09
+	db $01, $00, $00, $24, $0d
