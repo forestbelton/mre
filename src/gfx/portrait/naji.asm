99763c5c6aea1d614f9c0c616d114ce2c0ea812c
Forest Belton <65484+forestbelton@users.noreply.github.com>
2026-06-06 18:23:40 -0700

    Merge more portrait graphics into respective files

--- src/gfx/portrait/naji.asm
+++ src/gfx/portrait/naji.asm
@@ -6,18 +6,21 @@
 ; palettes (loaded by Func_18_6bc8 from $1a:$72e8/$7328) are carved here from the
 ; PNG's colour table.
 
-SECTION "NajiPortraitTiles", ROMX[$5ae8], BANK[$1a]
+SECTION "Naji graphics", ROMX
+
 NajiPortraitTiles:
 	INCBIN "assets/naji/tiles.bin"
 
 NajiPortraitPaletteBg:
 	INCBIN "assets/naji/palette_bg.bin"   ; 6 BG palettes (RGB555 LE)
 
-SECTION "NajiPortraitPaletteObj", ROMX[$7328], BANK[$1a]
+DS $10
+
 NajiPortraitPaletteObj:
 	INCBIN "assets/naji/palette_obj.bin"  ; 6 OBJ palettes (RGB555 LE)
 
-SECTION "NajiPortraitMapDesc", ROMX[$7368], BANK[$1a]
+DS $10
+
 NajiPortraitMapDesc:
 	db 11, 20
 	dw NajiPortraitAttrMap
@@ -28,3 +31,22 @@ NajiPortraitIndexMap:
 
 NajiPortraitAttrMap:
 	INCBIN "assets/naji/attrmap.bin"
+
+Data_1a_7526:
+	db $01, $03, $2f, $75, $2c, $75, $b4, $bc, $c4, $09, $09, $09, $0a, $00, $00, $00
+	db $08, $00, $08, $02, $08, $00, $10, $04, $08, $00, $18, $06, $08, $00, $20, $08
+	db $08, $10, $00, $0a, $09, $10, $08, $0c, $09, $10, $10, $0e, $09, $10, $18, $10
+	db $09, $10, $20, $12, $09, $01, $03, $64, $75, $61, $75, $56, $57, $5e, $09, $09
+	db $09, $0a, $00, $00, $00, $08, $00, $08, $02, $08, $00, $10, $04, $08, $00, $18
+	db $06, $08, $00, $20, $08, $08, $10, $00, $0a, $09, $10, $08, $34, $09, $10, $10
+	db $36, $09, $10, $18, $38, $09, $10, $20, $12, $09, $04, $00, $00, $14, $09, $00
+	db $08, $16, $09, $00, $10, $18, $09, $00, $18, $1a, $09, $06, $00, $00, $1c, $0a
+	db $00, $08, $1e, $0a, $00, $10, $20, $0a, $10, $00, $22, $0a, $10, $08, $24, $0a
+	db $10, $10, $26, $0a, $06, $00, $00, $28, $0a, $00, $08, $2a, $0a, $00, $10, $2c
+	db $0a, $10, $00, $2e, $0a, $10, $08, $30, $0a, $10, $10, $32, $0a, $03, $03, $e2
+	db $75, $d9, $75, $b4, $bc, $c4, $b5, $bd, $c5, $b6, $be, $c6, $09, $09, $09, $09
+	db $09, $09, $09, $09, $09, $03, $03, $fa, $75, $f1, $75, $5f, $62, $65, $60, $63
+	db $66, $61, $64, $67, $09, $09, $09, $09, $09, $09, $09, $09, $09, $0a, $00, $00
+	db $00, $08, $00, $08, $02, $08, $00, $10, $04, $08, $00, $18, $06, $08, $00, $20
+	db $08, $08, $10, $00, $0a, $09, $10, $08, $3a, $09, $10, $10, $3c, $09, $10, $18
+	db $3e, $09, $10, $20, $12, $09
--- src/gfx/portrait/toamuna.asm
+++ src/gfx/portrait/toamuna.asm
@@ -10,18 +10,21 @@
 ; relocatable. The 6 BG + 6 OBJ palettes (loaded by Toamuna_CheckSaveExists from
 ; $1a:$5800/$5840) are carved here from the PNG's colour table.
 
-SECTION "ToamunaPortraitTiles", ROMX[$4000], BANK[$1a]
+SECTION "Toamuna graphics", ROMX
+
 ToamunaPortraitTiles:
 	INCBIN "assets/toamuna/tiles.bin"    ; 384 tiles -> VRAM bank 1 $8000
 
 ToamunaPortraitPaletteBg:
 	INCBIN "assets/toamuna/palette_bg.bin"   ; 6 BG palettes (RGB555 LE)
 
-SECTION "ToamunaPortraitPaletteObj", ROMX[$5840], BANK[$1a]
+DS $10
+
 ToamunaPortraitPaletteObj:
 	INCBIN "assets/toamuna/palette_obj.bin"  ; 6 OBJ palettes (RGB555 LE)
 
-SECTION "ToamunaPortraitMapDesc", ROMX[$5880], BANK[$1a]
+DS $10
+
 ToamunaPortraitMapDesc:
 	db 11, 20                                      ; rows, cols
 	dw ToamunaPortraitAttrMap                       ; CGB attribute map pointer
@@ -32,3 +35,16 @@ ToamunaPortraitIndexMap:
 
 ToamunaPortraitAttrMap:
 	INCBIN "assets/toamuna/attrmap.bin"  ; 20x11 CGB BG attributes
+
+Data_1a_5a3e:
+	db $03, $03, $4d, $5a, $44, $5a, $a6, $ae, $b6, $a7, $af, $b7, $40, $48, $50, $08
+	db $08, $08, $08, $08, $08, $08, $08, $08, $0f, $00, $00, $00, $08, $00, $08, $02
+	db $08, $00, $10, $04, $08, $00, $18, $06, $08, $00, $20, $08, $08, $10, $00, $0a
+	db $08, $10, $08, $0c, $08, $10, $10, $0e, $08, $10, $18, $10, $08, $10, $20, $12
+	db $08, $20, $00, $14, $08, $20, $08, $16, $08, $20, $10, $08, $08, $20, $18, $08
+	db $08, $20, $20, $18, $08, $03, $03, $a2, $5a, $99, $5a, $56, $5e, $60, $57, $5f
+	db $61, $63, $64, $62, $08, $08, $08, $08, $08, $08, $08, $08, $08, $0f, $00, $00
+	db $00, $08, $00, $08, $02, $08, $00, $10, $04, $08, $00, $18, $06, $08, $00, $20
+	db $08, $08, $10, $00, $1a, $08, $10, $08, $1c, $08, $10, $10, $1e, $08, $10, $18
+	db $10, $08, $10, $20, $12, $08, $20, $00, $14, $08, $20, $08, $16, $08, $20, $10
+	db $08, $08, $20, $18, $08, $08, $20, $20, $18, $08
